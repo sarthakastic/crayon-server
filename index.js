8d5bd4a851968e9dd52b371b860e6457b9538e81
sarthakastic <iamsg26@gmail.com>
2024-02-09 19:50:18 +0530

    production config

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+const isDev = app.settings.env === "development";
+const URL = isDev ? "http://localhost:3000" : "https://the-crayon.vercel.app/";
+app.use(cors({ origin: URL }));
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: "http://localhost:3000",
+  cors: URL,
 });
 
 io.on("connection", (socket) => {
